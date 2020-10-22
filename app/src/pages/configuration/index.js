9ea5fda7b4b353b9fef4c16b8e7cb474834995d5
Sam Buters <sambuters@gmail.com>
2020-10-21 21:14:51 -0300

    Adding configuration page HTML and CSS

---
+++ app/src/pages/configuration/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
+import introImg from "../../assets/IntroImage.svg";
+import { useHistory } from "react-router-dom";
+import "./styles.css";
+function Configuration() {
+  return (
+    <div className="config-div">
+      <h3>Configuração</h3>
+      <form>
+        <h4>Defina o horário inicial</h4>
+
+        <div className="timeInput">
+          <label for="initial-hours">Horas:</label>
+          <input type="number" id="hours" min="0" max="23" placeholder="0"/>
+        </div>
+
+        <div className="timeInput">
+          <label for="initial-minutes">Minutos:</label>
+          <input type="number" id="initial-minutes" min="0" max="59" placeholder="0"/>
+        </div>
+
+        <h4>Defina o horário final</h4>
+
+        <div className="timeInput">        
+          <label for="final-hours">Horas:</label>
+          <input type="number" id="final-hours" min="0" max="23" placeholder="23"/>
+        </div>
+
+        <div className="timeInput">   
+          <label for="final-minutes">Minutos:</label>
+          <input type="number" id="final-minutes" min="0" max="59" placeholder="59"/>
+        </div>
+
+        <h4>Defina o acréscimo de tempo entre horários</h4>
+
+        <div className="timeInput">   
+          <label for="step-hours">Horas:</label>
+          <input type="number" id="step-hours" min="0" max="23" placeholder="1" />
+        </div>
+
+        <div className="timeInput">   
+          <label for="step-minutes">Minutos:</label>
+          <input type="number" id="step-minutes" step="30" max="59" placeholder="0" />
+        </div>
+
+      </form>
+    </div>
+  );
+}
+
+export default Configuration;
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Intro from "./pages/intro";
+import Configuration from "./pages/configuration";
 import Home from "./pages/home";
 
 export default function Routes() {
@@ -8,6 +9,7 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Intro} />
+        <Route path="/configuration" exact component={Configuration} />
         <Route path="/home" exact component={Home} />
       </Switch>
     </BrowserRouter>
